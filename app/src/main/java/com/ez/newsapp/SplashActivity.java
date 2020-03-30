5b52eab45d7f44873e24283f0776878cefa8f948
amey911 <amey.palav3@gmail.com>
2020-03-30 19:07:14 +0530

    splash screen

---
+++ app/src/main/java/com/ez/newsapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.ez.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setTheme(R.style.AppTheme);
+        this.getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable () {
+
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent (SplashActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
