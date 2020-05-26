e9a076a3cd5a4b8e5730ba5af43f224bbb2466ca
amey911 <amey.palav3@gmail.com>
2020-05-26 22:09:37 +0530

    LFT FUNCITION

--- app/src/main/java/com/ez/newsapp/HeckylActivity.java
+++ app/src/main/java/com/ez/newsapp/HeckylActivity.java
@@ -24,6 +24,7 @@ import com.ez.newsapp.Adapters.CustomAdapter;
 
 import com.ez.newsapp.HeckylApi.HeckylApiClient;
 import com.ez.newsapp.HeckylApi.HeckylInterface;
+import com.ez.newsapp.HeckylModels.Categories;
 import com.ez.newsapp.HeckylModels.HeckylNews;
 import com.ez.newsapp.HeckylModels.NewsItems;
 import com.ez.newsapp.Models.News;
@@ -41,6 +42,7 @@ public class HeckylActivity extends AppCompatActivity implements SwipeRefreshLay
     private RecyclerView heckylRecView;
     private RecyclerView.LayoutManager layoutManager;
    private List<NewsItems> newsItems = new ArrayList<>();
+   private List<Categories> categories = new ArrayList<>();
     private TextView topHeadline;
     private SwipeRefreshLayout swipeRefreshLayout;
 
@@ -52,11 +54,13 @@ public class HeckylActivity extends AppCompatActivity implements SwipeRefreshLay
     public static final String asset = "1";
     public static final String entitiytype = "ISIN";
     public static final String entitycode = "INE009A01021";
-    public static final String lft = "1448843117";
-//    public static final String sortby = "1";
+    public static String lft = "0";
+
+    String responseLft = "";
+
     private String sortby = "1";
 
-    // COUNTRY CODES :
+
 
     private static String sortByLatest = "1";
     private static String sortbyTrending = "2" ;
@@ -140,134 +144,158 @@ public class HeckylActivity extends AppCompatActivity implements SwipeRefreshLay
                 item.setChecked(true);
                 LoadRegionNews("16");
 
+                topHeadline.setText("News of Region: "+"Australia ");
+
                 break;
 
             case R.id.region_hongkong:
                 LoadRegionNews("92");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"HongKong ");
                 break;
 
             case R.id.region_india:
                 LoadRegionNews("1");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"India ");
                 break;
 
             case R.id.region_indonesia:
                 LoadRegionNews("95");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Indonesia ");
                 break;
 
                 case R.id.region_malaysia:
                     LoadRegionNews("122");
                     item.setChecked(true);
+                    topHeadline.setText("News of Region: "+"Malaysia ");
                 break;
 
             case R.id.region_singapore:
                 LoadRegionNews("181");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Singapore ");
                 break;
 
             case R.id.region_uk :
                 LoadRegionNews("211");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"UK ");
                 break;
 
             case R.id.region_us:
                 LoadRegionNews("2");
                 item.setChecked(true);
-
+                topHeadline.setText("News of Region: "+"US ");
                 break;
 
             case R.id.region_belgium:
                 LoadRegionNews("24");
-
+                topHeadline.setText("News of Region: "+"Belgium ");
                 break;
 
             case R.id.region_canada:
                 LoadRegionNews("3");
                 item.setChecked(true);
-
+                topHeadline.setText("News of Region: "+"Canada ");
                 break;
 
             case R.id.region_china:
                 LoadRegionNews("45");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"China ");
                 break;
 
             case R.id.region_denmark:
                 LoadRegionNews("57");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Denmark ");
                 break;
 
             case R.id.region_finland:
                 LoadRegionNews("70");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Finland ");
                 break;
 
             case R.id.region_iceland:
                 LoadRegionNews("94");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Iceland ");
                 break;
 
             case R.id.region_italy:
                 LoadRegionNews("100");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Italy ");
                 break;
 
             case R.id.region_japan:
                 LoadRegionNews("102");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Japan ");
                 break;
 
             case R.id.region_netherlands:
                 LoadRegionNews("145");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Netherlands ");
                 break;
 
 
             case R.id.region_norway:
                 LoadRegionNews("151");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Norway ");
                 break;
 
             case R.id.region_poland:
                 LoadRegionNews("161");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Poland ");
                 break;
 
             case R.id.region_portugal:
                 LoadRegionNews("162");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Portugal ");
                 break;
 
             case R.id.region_russia:
                 LoadRegionNews("167");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Russia ");
                 break;
 
             case R.id.region_southkorea:
                 LoadRegionNews("187");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"South Korea ");
                 break;
 
             case R.id.region_spain:
                 LoadRegionNews("188");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Spain ");
                 break;
 
             case R.id.region_sweden:
                 LoadRegionNews("193");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Sweden ");
                 break;
 
             case R.id.region_switzerland:
                 LoadRegionNews("194");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Switzerland ");
                 break;
 
             case R.id.region_germany:
                 LoadRegionNews("78");
                 item.setChecked(true);
+                topHeadline.setText("News of Region: "+"Germany ");
                 break;
 
         }
@@ -309,15 +337,34 @@ public class HeckylActivity extends AppCompatActivity implements SwipeRefreshLay
         call = heckylInterface.getNews(asset, entitiytype, entitycode, lft, sortKey);
 
          call.enqueue(new Callback<HeckylNews>() {
+
+
+
              @Override
              public void onResponse(Call<HeckylNews> call, Response<HeckylNews> response) {
+
+
+
+
+
+
                  if (response.isSuccessful() && response.body().getNewsItems() !=null){
 
                      if (newsItems.isEmpty()){
                          newsItems.clear();
                      }
 
+
+
                      newsItems = response.body().getNewsItems();
+                     responseLft = response.body().getLft();
+
+//                     lft = responseLft;
+
+                     Toast.makeText(HeckylActivity.this, "response lft: " +responseLft,  Toast.LENGTH_SHORT).show();
+
+
+
                      heckylAdapter = new CustomAdapter(newsItems, HeckylActivity.this);
                      heckylRecView.setAdapter(heckylAdapter);
                      heckylAdapter.notifyDataSetChanged();
@@ -327,6 +374,9 @@ public class HeckylActivity extends AppCompatActivity implements SwipeRefreshLay
 
 //                     topHeadline.setVisibility(View.INVISIBLE);
                      swipeRefreshLayout.setRefreshing(false);
+
+
+
 
                  }else {
 //                     topHeadline.setVisibility(View.INVISIBLE);
